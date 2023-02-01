12e2c7780c87636c2fc1427e147d28618366ebd9
Eunsung Cho <eunsung.cho@datacom.co.nz>
2023-02-01 17:44:04 +1300

    End of section 8

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,16 +1,23 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/products";
+import { useAppSelector } from "../../app/store/configureStore";
 import ProductCard from "./ProductCard";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface Props {
   products: Product[];
 }
 const ProductList = ({ products }: Props) => {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
   return (
     <Grid container spacing={4}>
       {products.map((p) => (
-        <Grid item xs={3} key={p.id}>
-          <ProductCard product={p} />
+        <Grid item xs={4} key={p.id}>
+          {!productsLoaded ? (
+            <ProductCardSkeleton />
+          ) : (
+            <ProductCard product={p} />
+          )}
         </Grid>
       ))}
     </Grid>
